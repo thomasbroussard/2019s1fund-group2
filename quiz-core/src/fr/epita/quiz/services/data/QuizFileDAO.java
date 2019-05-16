319d55e3c34213c6fcf68bc4562d57ea41dfbfe3
tbrou <tbrou@PCP-066.natsystem.fr>
2019-05-16 19:29:30 +0200

    * dao integration

---
+++ quiz-core/src/fr/epita/quiz/services/data/QuizFileDAO.java
@@ -0,0 +1,29 @@
+package fr.epita.quiz.services.data;
+
+import java.util.List;
+
+import fr.epita.quiz.datamodel.Quiz;
+
+public class QuizFileDAO {
+	
+	
+	public void create(Quiz quiz) {
+		
+	}
+	public void update(Quiz quiz) {
+		
+	}
+	public void delete(Quiz quiz) {
+		
+	}
+	public Quiz getById(int id) {
+		return null;
+		
+	}
+
+	public List<Quiz> search(Quiz quizCriterion) {
+	
+		return null;
+	}
+	
+}
